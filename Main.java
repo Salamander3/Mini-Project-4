a7267c0c9517d7a3df7b5eaaba5bdb7924cd736e
Salamander3 <benjamin.sentirmay@patelhs.org>
2021-01-29 13:07:53 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Main 
+{
+  //The main executing method
+  public static void main(String[] args) 
+  {
+    Scanner scan = new Scanner(System.in);
+    
+    
+  }
+
+  //This method 
+  public static boolean method1;
+  {
+
+  }
+
+  //This method
+  public static double method2;
+  {
+
+  }
+}
