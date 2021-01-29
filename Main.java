fae7f62101a1f58e367e773ec906583e7b754f04
Salamander3 <benjamin.sentirmay@patelhs.org>
2021-01-29 13:19:48 +0000

    Created first method that will randomize the number.

--- Main.java
+++ Main.java
@@ -10,11 +10,14 @@ class Main
     System.out.println("Enter a number between 1 and 10");
     int ogNum = scan.nextInt();
 
-
+    
   }
 
   //This method randomizes the number the user enters
-
+  static boolean numRandomizer(int inputNum);
+  {
+    return rNum = inputNum * 10 / 5 -10 + 16;
+  }
 
   //This method
   static double method2;
